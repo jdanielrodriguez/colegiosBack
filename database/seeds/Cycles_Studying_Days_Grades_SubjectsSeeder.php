dd3c1b1a949093b5501fb76b4ef3e3e411875950
Daniel Rodriguez <daniel@foxylabs.gt>
2017-08-23 23:56:04 -0600

    [ADDED] added seeders

---
+++ database/seeds/Cycles_Studying_Days_Grades_SubjectsSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Cycles_Studying_Days_Grades_SubjectsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
