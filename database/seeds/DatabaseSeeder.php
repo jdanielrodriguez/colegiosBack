a2c33b604274544da73b8e9f29c09bc619f9f150
Daniel Rodriguez <daniel@foxylabs.gt>
2017-08-23 23:55:37 -0600

    [UPDATED] database/seeds/DatabaseSeeder.php updated databaseseeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,25 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(CyclesSeeder::class);
+        $this->call(Studying_DaysSeeder::class);
+        $this->call(GradesSeeder::class);
+        $this->call(SubjectsSeeder::class);
+        $this->call(TeachersSeeder::class);
+        $this->call(EventsSeeder::class);
+        $this->call(TutorsSeeder::class);
+        $this->call(StudentsSeeder::class);
+        $this->call(UsersSeeder::class);
+        $this->call(InscriptionsSeeder::class);
+        $this->call(ChargesSeeder::class);
+        $this->call(Cycles_Studying_DaysSeeder::class);
+        $this->call(Cycles_Studying_Days_GradesSeeder::class);
+        $this->call(Inscriptions_Cycles_Studying_DaysSeeder::class);
+        $this->call(Cycles_Studying_Days_Grades_SubjectsSeeder::class);
+        $this->call(Cycles_Studying_Days_Grades_Subjects_TeachersSeeder::class);
+        $this->call(Subjects_StudentsSeeder::class);
+        $this->call(AssistanceSeeder::class);
+        $this->call(HomeworkSeeder::class);
+        $this->call(Tutors_StudentsSeeder::class);
     }
 }
