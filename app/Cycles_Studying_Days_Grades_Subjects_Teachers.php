60cedd621b43756ce6ed572a4ace02206200e1c8
Daniel Rodriguez <daniel@foxylabs.gt>
2017-09-12 16:33:12 -0600

    [UPDATED]  app/Cycles_Studying_Days_Grades_Subjects_Teachers.php - api subjects teachers

--- app/Cycles_Studying_Days_Grades_Subjects_Teachers.php
+++ app/Cycles_Studying_Days_Grades_Subjects_Teachers.php
@@ -9,4 +9,12 @@ class Cycles_Studying_Days_Grades_Subjects_Teachers extends Model
 {
     use SoftDeletes;
     protected $table = 'cycles_studying_days_grades_subjects_teachers';
+
+    public function subjects(){
+        return $this->hasOne('App\Cycles_Studying_Days_Grades_Subjects','id','csdgs')->with('subjects');
+    }
+
+    public function teachers(){
+        return $this->hasOne('App\Teachers','id','teacher');
+    }
 }
