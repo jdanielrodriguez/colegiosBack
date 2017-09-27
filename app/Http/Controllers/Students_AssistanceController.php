cb64c4f07927f9be158508fabc8f73b7cf426d2b
Daniel Rodriguez <daniel@foxylabs.gt>
2017-09-27 15:54:25 -0600

    [UDPATED] app/Http/Controllers/Students_AssistanceController.php - api assistance of students

--- app/Http/Controllers/Students_AssistanceController.php
+++ app/Http/Controllers/Students_AssistanceController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Http\Requests;
+use App\Students_Assistance;
+use Response;
+use Validator;
 class Students_AssistanceController extends Controller
 {
     /**
@@ -13,7 +16,7 @@ class Students_AssistanceController extends Controller
      */
     public function index()
     {
-        //
+        return Response::json(Students_Assistance::all(), 200);
     }
 
     /**
@@ -34,7 +37,47 @@ class Students_AssistanceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+       $validator = Validator::make($request->all(), [
+           'subject_student'          => 'required',
+           'assistance'          => 'required'
+       ]);
+       if ( $validator->fails() ) {
+           $returnData = array (
+               'status' => 400,
+               'message' => 'Invalid Parameters',
+               'validator' => $validator
+           );
+           return Response::json($returnData, 400);
+       }
+       else {
+           try {
+               $newObject = new Students_Assistance();
+               $newObject->subject_student            = $request->get('subject_student');
+               $newObject->assistance                 = $request->get('assistance');
+               $newObject->assistance_date                 = date('Y-m-d');
+               $newObject->save();
+               return Response::json($newObject, 200);
+           
+           } catch (\Illuminate\Database\QueryException $e) {
+               if($e->errorInfo[0] == '01000'){
+                   $errorMessage = "Error Constraint";
+               }  else {
+                   $errorMessage = $e->getMessage();
+               }
+               $returnData = array (
+                   'status' => 505,
+                   'SQLState' => $e->errorInfo[0],
+                   'message' => $errorMessage
+               );
+               return Response::json($returnData, 500);
+           } catch (Exception $e) {
+               $returnData = array (
+                   'status' => 500,
+                   'message' => $e->getMessage()
+               );
+               return Response::json($returnData, 500);
+           }
+       }
     }
 
     /**
@@ -45,7 +88,18 @@ class Students_AssistanceController extends Controller
      */
     public function show($id)
     {
-        //
+        $objectSee = Students_Assistance::find($id);
+        if ($objectSee) {
+            return Response::json($objectSee, 200);
+        
+        }
+        else {
+            $returnData = array (
+                'status' => 404,
+                'message' => 'No record found'
+            );
+            return Response::json($returnData, 404);
+        }
     }
 
     /**
@@ -68,7 +122,28 @@ class Students_AssistanceController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $objectUpdate = Students_Assistance::find($id);
+        if ($objectUpdate) {
+            try {
+                $objectUpdate->assistance = $request->get('assistance', $objectUpdate->assistance);
+        
+                $objectUpdate->save();
+                return Response::json($objectUpdate, 200);
+            } catch (Exception $e) {
+                $returnData = array (
+                    'status' => 500,
+                    'message' => $e->getMessage()
+                );
+                return Response::json($returnData, 500);
+            }
+        }
+        else {
+            $returnData = array (
+                'status' => 404,
+                'message' => 'No record found'
+            );
+            return Response::json($returnData, 404);
+        }
     }
 
     /**
@@ -79,6 +154,25 @@ class Students_AssistanceController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $objectDelete = Students_Assistance::find($id);
+        if ($objectDelete) {
+            try {
+                Students_Assistance::destroy($id);
+                return Response::json($objectDelete, 200);
+            } catch (Exception $e) {
+                $returnData = array (
+                    'status' => 500,
+                    'message' => $e->getMessage()
+                );
+                return Response::json($returnData, 500);
+            }
+        }
+        else {
+            $returnData = array (
+                'status' => 404,
+                'message' => 'No record found'
+            );
+            return Response::json($returnData, 404);
+        }
     }
 }
