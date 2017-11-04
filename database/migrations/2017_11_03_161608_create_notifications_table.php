55cfd2114f5a790e7bb25ac7033a87cd08f0febc
José Daniel <daniel@foxylabs.gt>
2017-11-04 00:46:48 -0600

    [ADDED] added migration for notifications

---
+++ database/migrations/2017_11_03_161608_create_notifications_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('message');
+            $table->integer('acepted')->default(1);
+            $table->integer('state')->default(3);
+            
+            $table->integer('affected')->unsigned()->nullable()->default(null);
+            $table->foreign('affected')->references('id')->on('students')->onDelete('cascade');
+            $table->integer('receiver')->unsigned()->nullable()->default(null);
+            $table->foreign('receiver')->references('id')->on('tutors')->onDelete('cascade');
+            $table->integer('sender')->unsigned()->nullable()->default(null);
+            $table->foreign('sender')->references('id')->on('teachers')->onDelete('cascade');
+
+            $table->softDeletes();
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
